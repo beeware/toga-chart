60c408e035b5d3072a1f011c9cfbd69b8528af49
Russell Keith-Magee <russell@keith-magee.com>
2022-08-19 06:43:50 +0800

    Small tweaks to example app.

--- examples/chart/app.py
+++ examples/chart/app.py
@@ -7,21 +7,22 @@ from toga.constants import COLUMN
 
 class ExampleChartApp(toga.App):
     def draw_chart(self, chart, figure, *args, **kwargs):
-        # example data
+        # Generate some example data
         mu = 100  # mean of distribution
         sigma = 15  # standard deviation of distribution
         x = mu + sigma * np.random.randn(437)
 
         num_bins = 50
 
+        # Add a subplot that is a histogram of the data,
+        # using the normal matplotlib API
         ax = figure.add_subplot(1, 1, 1)
-
-        # the histogram of the data
         n, bins, patches = ax.hist(x, num_bins, density=1)
 
         # add a 'best fit' line
         y = ((1 / (np.sqrt(2 * np.pi) * sigma)) * np.exp(-0.5 * (1 / sigma * (bins - mu))**2))
         ax.plot(bins, y, '--')
+
         ax.set_xlabel('Value')
         ax.set_ylabel('Probability density')
         ax.set_title(r'Histogram: $\mu=100$, $\sigma=15$')
@@ -47,7 +48,7 @@ class ExampleChartApp(toga.App):
 
 
 def main():
-    return ExampleChartApp('Chart', 'org.pybee.widgets.chart')
+    return ExampleChartApp('Chart', 'org.beeware.widgets.chart')
 
 
 if __name__ == '__main__':
