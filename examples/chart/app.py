9fcfbc21602c20f80ed38d8b5691987cb10dc15f
Sagi Shadur <saroad2@gmail.com>
2022-09-01 23:45:47 +0300

    Updated app application, so it will only create data once.
    Also added "recreate data" button that will create and display new data.

--- examples/chart/app.py
+++ examples/chart/app.py
@@ -6,21 +6,20 @@ from toga.constants import COLUMN
 
 
 class ExampleChartApp(toga.App):
+    mu = 100  # mean of distribution
+    sigma = 15  # standard deviation of distribution
+
     def draw_chart(self, chart, figure, *args, **kwargs):
-        # Generate some example data
-        mu = 100  # mean of distribution
-        sigma = 15  # standard deviation of distribution
-        x = mu + sigma * np.random.randn(437)
 
         num_bins = 50
 
         # Add a subplot that is a histogram of the data,
         # using the normal matplotlib API
         ax = figure.add_subplot(1, 1, 1)
-        n, bins, patches = ax.hist(x, num_bins, density=1)
+        n, bins, patches = ax.hist(self.x, num_bins, density=1)
 
         # add a 'best fit' line
-        y = ((1 / (np.sqrt(2 * np.pi) * sigma)) * np.exp(-0.5 * (1 / sigma * (bins - mu))**2))
+        y = ((1 / (np.sqrt(2 * np.pi) * self.sigma)) * np.exp(-0.5 * (1 / self.sigma * (bins - self.mu))**2))
         ax.plot(bins, y, '--')
 
         ax.set_xlabel('Value')
@@ -31,6 +30,7 @@ class ExampleChartApp(toga.App):
 
     def startup(self):
         np.random.seed(19680801)
+        self.recreate_data()
 
         # Set up main window
         self.main_window = toga.MainWindow(title=self.name)
@@ -40,11 +40,20 @@ class ExampleChartApp(toga.App):
         self.main_window.content = toga.Box(
             children=[
                 self.chart,
+                toga.Button("Recreate Data", on_press=self.on_recreate_press)
             ],
             style=Pack(direction=COLUMN)
         )
 
         self.main_window.show()
+
+    def recreate_data(self):
+        # Generate some example data
+        self.x = self.mu + self.sigma * np.random.normal(size=500)
+
+    def on_recreate_press(self, widget):
+        self.recreate_data()
+        self.chart.draw(self.chart.build_figure())
 
 
 def main():
