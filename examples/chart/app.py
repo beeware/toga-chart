797048644d1a045c358247ef6c9da90d6f94fc71
Russell Keith-Magee <russell@keith-magee.com>
2023-11-17 08:23:57 +0800

    Add sliders to control mu and sigma.

--- examples/chart/app.py
+++ examples/chart/app.py
@@ -7,12 +7,9 @@ import toga_chart
 
 
 class ExampleChartApp(toga.App):
-    MU = 100  # mean of distribution
-    SIGMA = 15  # standard deviation of distribution
-
     def set_data(self):
         # Generate some example data
-        self.x = self.MU + self.SIGMA * np.random.randn(437)
+        self.x = self.mu.value + self.sigma.value * np.random.randn(1000)
 
     def draw_chart(self, chart, figure, *args, **kwargs):
         num_bins = 50
@@ -20,17 +17,20 @@ class ExampleChartApp(toga.App):
         # Add a subplot that is a histogram of the data,
         # using the normal matplotlib API
         ax = figure.add_subplot(1, 1, 1)
-        n, bins, patches = ax.hist(self.x, num_bins, density=1)
+        n, bins, patches = ax.hist(self.x, num_bins, density=1, range=(0, 200))
 
         # add a 'best fit' line
-        y = (1 / (np.sqrt(2 * np.pi) * self.SIGMA)) * np.exp(
-            -0.5 * (1 / self.SIGMA * (bins - self.MU)) ** 2
+        y = (1 / (np.sqrt(2 * np.pi) * self.sigma.value)) * np.exp(
+            -0.5 * (1 / self.sigma.value * (bins - self.mu.value)) ** 2
         )
         ax.plot(bins, y, "--")
 
         ax.set_xlabel("Value")
         ax.set_ylabel("Probability density")
-        ax.set_title(r"Histogram: $\mu=100$, $\sigma=15$")
+        ax.set_title(
+            rf"Histogram: $\mu={self.mu.value:.1f}$, "
+            rf"$\sigma={self.sigma.value:.1f}$"
+        )
 
         figure.tight_layout()
 
@@ -39,18 +39,44 @@ class ExampleChartApp(toga.App):
         self.chart.redraw()
 
     def startup(self):
-        np.random.seed(19680801)
-        self.set_data()
-
         # Set up main window
         self.main_window = toga.MainWindow()
 
         self.chart = toga_chart.Chart(style=Pack(flex=1), on_draw=self.draw_chart)
 
+        self.mu = toga.Slider(
+            value=100,
+            min=0,
+            max=200,
+            on_change=self.recreate_data,
+            style=Pack(flex=1),
+        )
+        self.sigma = toga.Slider(
+            value=15,
+            min=1,
+            max=30,
+            on_change=self.recreate_data,
+            style=Pack(flex=1),
+        )
+
+        self.set_data()
+
         self.main_window.content = toga.Box(
             children=[
                 self.chart,
-                toga.Button("Recreate data", on_press=self.recreate_data),
+                toga.Box(
+                    children=[toga.Label("𝜇"), self.mu],
+                    style=Pack(padding=(5, 10)),
+                ),
+                toga.Box(
+                    children=[toga.Label("𝜎"), self.sigma],
+                    style=Pack(padding=(5, 10)),
+                ),
+                toga.Button(
+                    "Recreate data",
+                    on_press=self.recreate_data,
+                    style=Pack(padding=(10, 10, 20, 10)),
+                ),
             ],
             style=Pack(direction=COLUMN),
         )
