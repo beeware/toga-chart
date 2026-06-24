411d0aedc2a812e9d4247cadf97472e1227b4715
Russell Keith-Magee <russell@keith-magee.com>
2026-06-24 08:40:30 +0800

    Apply changes required by setuptools_scm update.

--- src/toga_chart/__init__.py
+++ src/toga_chart/__init__.py
@@ -1,22 +1,7 @@
+from importlib.metadata import version
+
 from .chart import Chart
 
-try:
-    # Read version from SCM metadata
-    # This will only exist in a development environment
-    from setuptools_scm import get_version
-
-    # Excluded from coverage because a pure test environment (such as the one
-    # used by tox in CI) won't have setuptools_scm
-    __version__ = get_version("../..", relative_to=__file__)  # pragma: no cover
-except (ModuleNotFoundError, LookupError):
-    # If setuptools_scm isn't in the environment, the call to import will fail.
-    # If it *is* in the environment, but the code isn't a git checkout (e.g.,
-    # it's been pip installed non-editable) the call to get_version() will fail.
-    # If either of these occurs, read version from the installer metadata.
-
-    # importlib.metadata.version was added in Python 3.8
-    from importlib.metadata import version
-
-    __version__ = version("toga_chart")
+__version__ = version("toga_chart")
 
 __all__ = ["Chart"]
